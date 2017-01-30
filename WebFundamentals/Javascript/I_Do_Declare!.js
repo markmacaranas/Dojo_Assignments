421a141adf4591c7bac71936d31c864de263abc8
Mark Macaranas <markmacaranas@gmail.com>
2017-01-30 08:52:51 -0800

    add I Do Declare for Javascript

---
+++ WebFundamentals/Javascript/I_Do_Declare!.js
@@ -0,0 +1,26 @@
+var num1 = 7;
+var num2 = -1;
+var num3 = 0.4;
+var num4 = 100;
+
+var string1 = "Mark";
+var string2 = "coding";
+var string3 = "desk";
+
+var doIcode = true;
+var doInotcode = false;
+
+var x;
+
+  console.log("The first num1:", num1);
+  console.log("The first num2:", num2);
+  console.log("The first num3:", num3);
+  console.log("The first num4:", num4);
+
+  console.log("My name is", string1);
+  console.log("I love", string2, "at my", string3);
+
+  console.log(doIcode);
+  console.log(doInotcode);
+
+  console.log(x);
