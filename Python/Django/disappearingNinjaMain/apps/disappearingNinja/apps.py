c4fda88d107033e3ff55261ab439840693413975
Mark Macaranas <markmacaranas@gmail.com>
2017-02-23 10:28:32 -0800

    update

--- Python/Django/disappearingNinjaMain/apps/disappearingNinja/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class DisappearingninjaConfig(AppConfig):
-    name = 'disappearingNinja'
--- Python/Django/disappearingNinjaMain/apps/disappearingNinja/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^ninjas$',views.all_ninjas),
-    url(r'^ninjas/(?P<color>\w+)$',views.one_ninja)
-]
